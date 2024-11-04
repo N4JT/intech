37d6e258b1b9a0bd76d8260269c0d6b1f4484125
N4JT <nightquicker@gmail.com>
2024-11-04 12:18:42 +0100

    added implementation of admin role

--- pages/components/menu.php
+++ pages/components/menu.php
@@ -1,5 +1,5 @@
 <?php
-
+include './../functions/utils/getUser.php';
 ?>
 <body>
 <div class="menu-container">
@@ -8,6 +8,14 @@
         <a href= "./remove.php"  class="remove" >ODOBER</a>
         <a href= "./edit.php " class="edit" >UPRAV</a>
         <a href= "./dashboard.php " class= "showall" >ZOBRAZ VSETKY</a>
+
+        <?php
+        if($role == "admin"){
+            echo " <a href=./admin.php class=admin>USERS</a>";
+        }
+        
+        
+        ?> 
         <a href="./logout.php" class="logout">ODLHAS SA</a>
     </div>
 </div> 
