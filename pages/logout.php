9b592d038bc47f7911e446ef314513837c033e23
N4JT <nightquicker@gmail.com>
2024-11-11 19:02:45 +0100

    added logout

--- pages/logout.php
+++ pages/logout.php
@@ -1,11 +1,17 @@
 <?php
 
-
+include "./components/head.php";
 ?>
 
 <body>
-    <h1>U SURE U WANT TO LOGOUT?</h1>
-    <form method="POST" action="./../functions/database/logout.php">
+    <div class="data-container">
+        <div class="data-content">
+        
+    <form class="data-form" method="POST" action="./../functions/database/logout.php">
+    <h1 class="nadpis">Naozaj sa chceš odhlásiť?</h1>
     <input type='submit' value='LOGOUT'>
     </form>
+        </div>
+    </div>
+    
 </body>
