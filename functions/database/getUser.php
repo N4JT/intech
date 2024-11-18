47be38053cbfa5d873f0b48c0d3370c7eed8023c
N4JT <nightquicker@gmail.com>
2024-11-18 16:34:56 +0100

    added design for remove user

---
+++ functions/database/getUser.php
@@ -0,0 +1,22 @@
+<?php
+require("db.php"); 
+
+
+
+function getUser($userId){ 
+    global $conn;
+    $sql = "SELECT * FROM users where id = $userId";
+    $result = $conn->query($sql); 
+    if ($result->num_rows > 0) {
+        while ($row = $result->fetch_assoc()) {
+            $results[] = $row; 
+        }
+        return $results;
+    }else{
+        return null;
+    }
+}
+   
+
+
+?>
--- pages/removeUser.php
+++ pages/removeUser.php
@@ -1,15 +1,25 @@
 <?php
+include "./components/head.php";
+require('./../functions/database/getUser.php');
+
 $selecteduserId = $_GET["user_id"] ?? null;
+if(isset($selecteduserId)){
+    $user = getUser($selecteduserId); 
+}
 ?>
 
 <body>
+<div class="data-container">
+<div class="data-content">
 <form id="edituserForm" class="data-form" action="./../functions/database/admin/removeUser.php" method="POST">
-            <label for="userSelect">Si si isty, ze chces odstranit pouzivatela?</label>
-
+            <h3 for="userSelect">Si si isty, ze chces odstranit pouzivatela</h3>
+            <h1 class="NADPIS"><?php echo $user[0]["username"] ?></h1>
             <input type="hidden" name="user_id" id="user_id" value=<?php echo $selecteduserId?>>
             
-            <input type="submit" value="remove user">
+            <input type="submit" value="ODSTRANIT">
         </form>
 
-      
+</div>
+</div>
+
 </body>
